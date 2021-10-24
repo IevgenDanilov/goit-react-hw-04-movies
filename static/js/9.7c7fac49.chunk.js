d74ed5787d573c0c5db32abf280daf1a94ea3a30
Yevhen Danilov <danilov.evgen@gmail.com>
2021-10-24 21:21:36 +0300

    Updates

---
+++ static/js/9.7c7fac49.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-04-movies"]=this["webpackJsonpgoit-react-hw-04-movies"]||[]).push([[9],{87:function(e,t,n){"use strict";n.r(t),n.d(t,"default",(function(){return r}));var i=n(1),r=function(){return Object(i.jsxs)("h1",{style:{textAlign:"center"},children:["Sorry, page not found",Object(i.jsx)("span",{role:"img","arial-label":"emoji",children:"\ud83d\ude02"})]})}}}]);
+//# sourceMappingURL=9.7c7fac49.chunk.js.map
