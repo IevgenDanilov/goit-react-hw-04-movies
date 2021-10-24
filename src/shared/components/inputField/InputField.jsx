d8f820ac4e94d223bb8b0bb3f063a6bad0de7509
Yevhen Danilov <danilov.evgen@gmail.com>
2021-10-24 21:21:04 +0300

    hw5 compleated upd

--- src/shared/components/inputField/InputField.jsx
+++ src/shared/components/inputField/InputField.jsx
@@ -1,12 +1,16 @@
 import { generate } from "shortid";
+import styles from "./InputField.module.scss";
 
 const InputField = ({ label, ...field }) => {
   const id = generate();
 
   return (
-    <div className="form-group">
+    <div className={styles.formGroup}>
       {label && <label htmlFor={id}></label>}
-      <input {...field} id={id} />
+      <input {...field} id={id} className={styles.input} />
+      <button className={styles.btn} type="submit">
+        Пошук
+      </button>
     </div>
   );
 };
